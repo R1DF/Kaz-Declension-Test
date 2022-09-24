f5d74b45a70f988ac6a8bd310de7929e29c152d2
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-24 14:34:08 +0600

    added genitive test

--- src/main.py
+++ src/main.py
@@ -5,17 +5,30 @@ Generates a random sentence containing a noun using a chosen case by the user.
 
 # Imports
 import os
+import random
 from data_getter import json_data
+from tests.genitive import GenitiveTest
 
 # Clear function
 def clear():
     os.system("clear" if os.name != "nt" else "cls")
 
+def make_genitive():
+    clear()
+    tester = GenitiveTest()
+    if random.randint(1, 2) == 1:
+        tester.make_normal()
+    else:
+        tester.make_special()
+    print(F"GENITIVE CASE\n{tester.description}\n")
+    tester.print_sentence()
+    input(f"\"Parent\" noun: {tester.parent_before}\n\"Child\" noun: {tester.child_before}\n\nPress Enter to return.\n\n")
+
 # Main code
 os.system("title Kazakh Declension Test")
 while True:
     clear()  # Must always clear first
-    print("Kazakh Declension test\nSelect a case to generate:")
+    print("Kazakh Declension test\nSelect a case to generate a sentence from:")
     for noun_case in range(len(json_data["cases"])):
         print(f"{noun_case + 1}. {json_data['cases'][noun_case]}")
     print("8. Quit")
@@ -24,17 +37,22 @@ while True:
 
     # Validation
     if not user_input.isnumeric():
-        print("Please enter a numeric value.")
+        input("Please enter a numeric value.\n")
         continue
     else:
         user_input = int(user_input)
         if user_input < 1 or user_input > 8:
-            print("Please enter a value in the range.")
+            input("Please enter a value in the range.\n")
             continue
 
-    # If the option is to quit
-    if user_input == 8:
-        break
+    # Options
+    match user_input:
+        case 1:
+            pass
+        case 2:
+            make_genitive()
+        case 8:
+            break
 
 # Quit scenario
 clear()
