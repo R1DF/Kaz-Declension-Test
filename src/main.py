8390880f45b2d3b9c874dff7b6a8dfdd10b8b031
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-25 15:04:29 +0600

    added locative, ablative, and instrumental tests

--- src/main.py
+++ src/main.py
@@ -11,6 +11,9 @@ from tests.nominative import NominativeTest
 from tests.genitive import GenitiveTest
 from tests.dative import DativeTest
 from tests.accusative import AccusativeTest
+from tests.locative import LocativeTest
+from tests.ablative import AblativeTest
+from tests.instrumental import InstrumentalTest
 
 # Clear function
 def clear():
@@ -58,6 +61,39 @@ def make_accusative():
     tester.print_sentence()
     input(f"Original noun: {tester.word_before}\n\nPress Enter to return.\n\n")
 
+def make_locative():
+    clear()
+    tester = LocativeTest()
+    if random.randint(1, 2) == 1:
+        tester.make_normal()
+    else:
+        tester.make_special()
+    print(f"LOCATIVE CASE\n{tester.description}\n")
+    tester.print_sentence()
+    input(f"Original noun: {tester.word_before}\n\nPress Enter to return.\n\n")
+
+def make_ablative():
+    clear()
+    tester = AblativeTest()
+    if random.randint(1, 2) == 1:
+        tester.make_normal()
+    else:
+        tester.make_special()
+    print(f"ABLATIVE CASE\n{tester.description}\n")
+    tester.print_sentence()
+    input(f"Original noun: {tester.word_before}\n\nPress Enter to return.\n\n")
+
+def make_instrumental():
+    clear()
+    tester = InstrumentalTest()
+    if random.randint(1, 2) == 1:
+        tester.make_normal()
+    else:
+        tester.make_special()
+    print(f"INSTRUMENTAL CASE\n{tester.description}\n")
+    tester.print_sentence()
+    input(f"Original noun: {tester.word_before}\n\nPress Enter to return.\n\n")
+
 
 # Main code
 try:
@@ -89,6 +125,12 @@ try:
                 make_dative()
             case 4:
                 make_accusative()
+            case 5:
+                make_locative()
+            case 6:
+                make_ablative()
+            case 7:
+                make_instrumental()
             case 8:
                 break
 
